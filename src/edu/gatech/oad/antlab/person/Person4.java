fc91cd7950073865d04b8a9746ed11d80a544282
Derrek Keyser <Derrek Keyser@Derreks-PC>
2012-10-01 22:58:54 -0400

    #added calc method to Person 4

--- src/edu/gatech/oad/antlab/person/Person4.java
+++ src/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Derrek Keyser
  *  @version 1.1
  */
 public class Person4 {
@@ -30,7 +30,20 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
+    	char[] x = input.toCharArray();
+        
+        for(int i = 0; i > x.length; i++){
+      	  int num1 = (int) (Math.random() * x.length);
+      	  int num2 = (int) (Math.random() * x.length);
+      	  char temp1 = x[num1];
+      	  char temp2 = x[num2];
+      	  
+      	  x[num1] = temp2;
+      	  x[num2] = temp1;
+        }
+        
+        String endString = new String(x);
+        return endString;
       return null;
     }
     
